b641db2c2254aa1ca9d288ad344bb6c3b2cfbc0a
DevRaj951515 <devraj951515@gmail.com>
2026-03-27 18:39:53 +0530

    Fix language statistics and shorten README

--- flask_server.py
+++ flask_server.py
@@ -93,4 +93,4 @@ if __name__ == "__main__":
     if not os.path.exists('screenshots'):
         os.makedirs('screenshots')
     
-    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=7860, debug=False, threaded=True)
